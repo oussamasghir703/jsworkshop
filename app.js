a4d0d490a98fc78bcfab5194a1cc4c13795b1492
oussamasghir <oussamasghir703@gmail.com>
2023-06-14 11:06:21 +0100

    add divide function

---
+++ app.js
@@ -0,0 +1,13 @@
+function add(a,b){
+	return a+b
+}
+console.log(add(3,8))
+function  multiply(a,b){
+	return a*b;
+}
+console.log(multiply(3,8))
+function divide(a,b){
+	if (b!==0)
+		return a/b;
+}
+console.log(divide(3,8))
